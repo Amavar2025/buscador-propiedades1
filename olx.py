33a32ae9452be000909a9fb738c41ed1ff1235d3
Amavar2025 <angela.martinvar@gmail.com>
2025-07-09 22:51:36 -0500

    Update olx.py

--- olx.py
+++ olx.py
@@ -1,3 +1,13 @@
-# olx.py
-
-# Código del scraper o configuración aquí...
+metrocuadrado.py ✅
+properati.py ✅
+finca_raiz.py ✅
+ciencuadras.py ⛔ (es el que estamos corrigiendo ahora)
+lahaus.py
+olx.py
+londono.py
+onegrupo.py
+acrecer.py
+albertoalvarez.py
+conaltura.py
+mabelochoa.py
+umbral.py
