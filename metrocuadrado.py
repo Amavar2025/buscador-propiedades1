a07e7a575340d50f0a80a0157ab7762dd36ac4f4
Amavar2025 <angela.martinvar@gmail.com>
2025-07-09 22:54:41 -0500

    Update metrocuadrado.py

--- metrocuadrado.py
+++ metrocuadrado.py
@@ -4,7 +4,7 @@ def buscar(barrio, habitaciones, precio_max):
             "Título": "Apartamento en " + barrio,
             "Habitaciones": habitaciones,
             "Precio (millones)": precio_max,
-            "Fuente": "Metrocuadrado",
-            "Link": "https://www.metrocuadrado.com"
+            "Fuente": "Ciencuadras",
+            "Link": "https://www.ciencuadras.com"
         }
     ]
